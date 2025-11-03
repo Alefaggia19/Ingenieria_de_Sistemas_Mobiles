f197b0602dfd96997ec1983d2cd8b74e60b882e4
Alefaggia19 <p52fafaa@uco.es>
2025-11-03 18:34:26 +0100

    aggiunta e aggiornamento

---
+++ src/test/java/com/checkit/checkit_backend/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.checkit.checkit_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
