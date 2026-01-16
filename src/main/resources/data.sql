88a54990288b2ad468317d5bdc10e7281da80e54
Carlos Raigon <i12rasec@uco.es>
2026-01-16 14:42:27 +0100

    consulta sql para probar desafios

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,7 @@
 INSERT INTO USERS (realname,password,email)
 VALUES
 ('admin','$2a$12$Akz1PcXyaQhFpTEWSF2A6e92PxYiPrfOihVz/NVpiteGQWJO3OQ8K','admin@checkit.com');
+
+INSERT INTO users (realname, email, password) VALUES ('Ana Gómez', 'ana@example.com', '$2a$10$8.UnVuG9HHgffUDAlk8qfOuVGkqRzgVymGe07xd00DMxs.TVuHOnu');
+
+INSERT INTO challenges (name, description, is_ordered, user_id, creation_date) VALUES ('Ruta Romana', 'Descubre el legado romano de Córdoba.', true, 1, CURRENT_TIMESTAMP);
